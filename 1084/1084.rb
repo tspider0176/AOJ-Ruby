2c076ac4b8a7f3cef2f77eed0f6962e54b75b3a6
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-01-30 18:08:21 +0900

    WIP 1084

--- 1084/1084.rb
+++ 1084/1084.rb
@@ -1,32 +1,10 @@
-# http://yuroyoro.hatenablog.com/entry/2012/08/09/203959
-module ComposableFunction
-  def compose(g)
-    lambda{|*args| self.to_proc.call(g.to_proc.call(*args)) }
-  end
-
-  def >>(g)
-    g << self
-  end
-
-  def self.included(klass)
-    klass.send(:alias_method, :<<, :compose)
-  end
-end
-
-[Proc, Method, Symbol].each do |klass|
-  klass.send(:include, ComposableFunction)
-end
-
 n, k = STDIN.gets.chomp.split(' ')
-
-f = :chomp.to_proc
-g = :to_i.to_proc
-h = f >> g
 
 cs = (1..n.to_i).map{
   STDIN.gets
-}.map(&h)
+}.map(&:chomp).map(&:to_i)
 
-p cs
+# k区切りの積の配列へ変換しc'を求める
+cd = cs[0..-2].zip(cs[1..-1]).map{|f, x| f * x}.max
 
-# k区切りの席の配列へ変換
+#
