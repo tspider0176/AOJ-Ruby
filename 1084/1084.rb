bb631c5bfbc31a05b0b83baaf608a648c28ae20b
Tomohiro Saito <s1210176@stdrsv2122.u-aizu.ac.jp>
2017-01-31 23:12:12 +0900

    WIP 1084

--- 1084/1084.rb
+++ 1084/1084.rb
@@ -1,10 +1,26 @@
-n, k = STDIN.gets.chomp.split(' ')
+def mulNextWithIndex(list, k)
+  (0..(list.length - k)).map{|m| [m, list[m..(m + k - 1)].inject(:*)]}.to_h
+end
 
-cs = (1..n.to_i).map{
-  STDIN.gets
-}.map(&:chomp).map(&:to_i)
+def maxValInHash(hash)
+  hash.map{|k, v| v}.max
+end
 
-# k区切りの積の配列へ変換しc'を求める
-cd = cs[0..-2].zip(cs[1..-1]).map{|f, x| f * x}.max
+loop do
+  n, k = STDIN.gets.chomp.split(' ').map(&:to_i)
+  if n == 0 && k == 0 then break end
+  
+  cs = (1..n.to_i).map{
+    STDIN.gets
+  }.map(&:chomp).map(&:to_i)
+  
+  csd = mulNextWithIndex(cs, k)
+  cd = maxValInHash(csd)
 
-#
+  max_index = csd.select{|k, v| v == cd}.to_a.flatten[0]
+  max_arr = cs[max_index..(max_index+k-1)]
+  max_arr.delete_at(max_arr.find_index(max_arr.min))
+  ck = max_arr.push((cs - max_arr).max).inject(:*)  
+  
+  puts ck - cd < 0 ? "NO GAME" : (ck - cd)
+end
