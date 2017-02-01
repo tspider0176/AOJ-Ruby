a8a32060d435704929f7ec04a72f5c8144960ee5
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-02-01 18:06:22 +0900

    WIP

--- 1084/1084.rb
+++ 1084/1084.rb
@@ -1,10 +1,19 @@
+
+def mulNext(list, n)
+  # listについて隣り合うn個の積を求め返す関数
+  (1..(list.length - n + 1)).map{|m|
+    list[0..m]
+  }
+end
+
+
 n, k = STDIN.gets.chomp.split(' ')
 
 cs = (1..n.to_i).map{
   STDIN.gets
 }.map(&:chomp).map(&:to_i)
 
-# k区切りの積の配列へ変換しc'を求める
+# 2区切りの積の配列へ変換しc'を求める
 cd = cs[0..-2].zip(cs[1..-1]).map{|f, x| f * x}.max
 
 #
